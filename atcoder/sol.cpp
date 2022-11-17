ce946a52e167dc006c9a148b41dae96b388b8ef7
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-11-17 17:29:15 +0900

    Implement basic code template for atcoder

---
+++ atcoder/sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    
+    solve();
+
+    return 0;
+}
