1b75b18d31d8367007d23d27fbc4a06833972844
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-11-28 19:32:06 +0900

    Solve codeforces, contest 1764, problem b
    problem link: https://codeforces.com/problemset/problem/1764/b

---
+++ codeforces/1764/b/sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    int agcd = a[0];
+    for (int i = 1; i < n; ++i) {
+        agcd = gcd(agcd, a[i]);
+    }
+
+    cout << a.back() / agcd << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
