89cc6b43bfd8cdef8164915907897b3f9fa5a15f
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-12-16 17:32:00 +0900

    Solve codeforces, contest 1760, problem b
    problem link: https://codeforces.com/problemset/problem/1760/b

---
+++ codeforces/1760/b/sol.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int ans = 0;
+    for (char ch : s) {
+        ans = max(ans, ch - 'a' + 1);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
