36d69de0338672d68f76f329a498da7b643a4c93
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-12-23 01:50:40 +0900

    Solve codeforces, contest 1763, problem a
    problem link: https://codeforces.com/problemset/problem/1763/a

---
+++ codeforces/1763/a/sol.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    int mx = a[0], mn = a[0];
+    for (int i = 1; i < n; ++i) {
+        mx |= a[i];
+        mn &= a[i];
+    }
+
+    cout << mx - mn << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
