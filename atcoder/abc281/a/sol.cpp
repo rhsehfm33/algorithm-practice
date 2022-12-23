3bdf75e30e1bf86779959a446a1014fa8609c31e
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-12-24 01:36:08 +0900

    Solve atcoder, contest abc281, problem a
    problem link: https://atcoder.jp/contests/abc281/tasks/abc281_a

---
+++ atcoder/abc281/a/sol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+    int N;
+    cin >> N;
+    while (N >= 0) {
+        cout << N-- << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    
+    solve();
+
+    return 0;
+}
