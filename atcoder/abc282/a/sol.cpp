5bd45089882e2928675cf6ff8cfdb97e5fb28109
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-01-04 16:02:38 +0900

    Solve atcoder, contest abc282, problem a
    problem link: https://atcoder.jp/contests/abc282/tasks/abc282_a

---
+++ atcoder/abc282/a/sol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve() {
+    int K;
+    cin >> K;
+    for (int i = 0; i < K; ++i) {
+        cout << char('A' + i);
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    
+    solve();
+
+    return 0;
+}
