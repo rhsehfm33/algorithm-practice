61e1dab18217a2501e92b5a2086b8360d0d45456
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-04-03 15:28:59 +0900

    Solve codeforces, contest 1810, problem a
    problem link: https://codeforces.com/problemset/problem/1810/a

---
+++ c++/codeforces/1810/a/sol.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (a[i] <= i + 1) {
+            cout << "YES\n";
+            return;
+        }
+    }
+
+    cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int _ti = 1; _ti <= T; ++_ti) {
+        solve(_ti);
+    }
+}
