6c5e63352022130c35f9631c42fa8e398f948483
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-06-06 20:23:26 +0900

    Solve codeforces, contest 1838, problem a
    problem link: https://codeforces.com/problemset/problem/1838/a

---
+++ c++/codeforces/1838/a/sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    sort(a.begin(), a.end());
+    if (a[0] < 0) {
+        cout << a[0] << '\n';
+    }
+    else {
+        cout << a.back() << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int _ti = 1; _ti <= T; ++_ti) {
+        solve(_ti);
+    }
+}
