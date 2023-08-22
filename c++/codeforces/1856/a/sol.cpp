7d88c9b166e72416e1dbde9ba7800f87003daf75
rhsehfm33 <rhsehfm33@gmail.com>
2023-08-23 00:04:03 +0900

    Solve codeforces, contest 1856, problem a
    problem link: https://codeforces.com/problemset/problem/1856/a

---
+++ c++/codeforces/1856/a/sol.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        if (a[i] > a[i + 1]) {
+            ans = max(ans, a[i]);
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
