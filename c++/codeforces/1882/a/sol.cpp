431499ed19f2ebef42157643bb8143d1d7cd6a40
rhsehfm33 <rhsehfm33@gmail.com>
2023-09-30 13:05:04 +0900

    Solve codeforces, contest 1882, problem a
    problem link: https://codeforces.com/problemset/problem/1882/a

---
+++ c++/codeforces/1882/a/sol.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ++ans;
+        
+        if (a[i] == ans) {
+            ++ans;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
