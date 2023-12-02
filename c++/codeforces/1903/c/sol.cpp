2f812ed76b977a842f285aa6cf456820fc033641
rhsehfm33 <rhsehfm33@gmail.com>
2023-12-02 21:59:13 +0900

    Solve codeforces, contest 1903, problem c
    problem link: https://codeforces.com/problemset/problem/1903/c

---
+++ c++/codeforces/1903/c/sol.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve(int _ti) {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    ll ans = 0;
+    for (ll i = n - 1, sum = 0; i >= 0; --i) {
+        if (sum > 0) {
+            ans += sum;
+        }
+        ans += a[i];
+        sum += a[i];
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
