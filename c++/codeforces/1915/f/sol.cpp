13464d971f65a4b1317d388e1acc684fe1589841
rhsehfm33 <rhsehfm33@gmail.com>
2024-01-06 11:26:49 +0900

    Solve codeforces, contest 1915, problem f
    problem link: https://codeforces.com/problemset/problem/1915/f

---
+++ c++/codeforces/1915/f/sol.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve(int _ti) {
+    int n;
+    cin >> n;
+    vector<array<int, 2>> p(n * 2);
+    for (int i = 0; i < 2 * n; ++i) {
+        cin >> p[i][0];
+        p[i][1] = i % 2 ? -1 : 1;
+    }
+    sort(p.begin(), p.end());
+
+    ll ans = 0, cur = 0;
+    for (auto [p0, p1] : p) {
+        if (p1 == -1) {
+            ans += --cur;
+        } else {
+            ++cur;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
