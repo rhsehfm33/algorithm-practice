b9ae57d6ce56597701fa214fb4a4300d103e6d09
rhsehfm33 <rhsehfm33@gmail.com>
2024-01-09 23:24:01 +0900

    Solve codeforces, contest 1905, problem a
    problem link: https://codeforces.com/problemset/problem/1905/a

---
+++ c++/codeforces/1905/a/sol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+    int n, m;
+    cin >> n >> m;
+    cout << max(n, m) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
