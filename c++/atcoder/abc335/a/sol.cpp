d6ad9c81a0309294b0cba1465a8b69740f0ce131
rhsehfm33 <rhsehfm33@gmail.com>
2024-01-13 17:34:54 +0900

    Solve atcoder, contest abc335, problem a
    problem link: https://atcoder.jp/contests/abc335/tasks/abc335_a

---
+++ c++/atcoder/abc335/a/sol.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve() {
+    string s;
+    cin >> s;
+    s.back() += 1;
+    cout << s << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    
+    solve();
+
+    return 0;
+}
