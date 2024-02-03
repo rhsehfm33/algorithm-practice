48d217f2328eee23f2172a84f15a1eafa44ba9a8
rhsehfm33 <rhsehfm33@gmail.com>
2024-02-03 13:54:43 +0900

    Set basic sol.cpp template

---
+++ c++/sol.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int _ti) {
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int ti = 1; ti <= T; ++ti) {
+        solve(ti);
+    }
+
+    return 0;
+}
