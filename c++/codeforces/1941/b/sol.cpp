0b11aaa089cb9a4133dc3df96baab045315e7b1e
rhsehfm33 <rhsehfm33@gmail.com>
2024-03-16 23:25:54 +0900

    Solve codeforces, contest 1941, problem b
    problem link: https://codeforces.com/problemset/problem/1941/b

---
+++ c++/codeforces/1941/b/sol.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int _ti;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    for (int i = 0; i < n - 2 and a[i] >= 0; ++i) {
+        a[i + 1] -= a[i] * 2;
+        a[i + 2] -= a[i];
+        a[i] = 0;
+    }
+
+    for (auto& ai : a) {
+        if (ai) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (_ti = 1; _ti <= T; ++_ti) {
+        solve();
+    }
+
+    return 0;
+}
