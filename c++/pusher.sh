7a44cfbba5b334bd4e732592a352253fcb01cf34
rhsehfm33 <rhsehfm33@gmail.com>
2025-09-07 01:15:28 +0000

    Refactor git commit & push script

--- c++/pusher.sh
+++ c++/pusher.sh
@@ -4,17 +4,19 @@ cd /home || exit 1
 
 commit_made=false
 
-git reset
+git reset > /dev/null 2>&1
 
 while read -r file; do
-    if [[ "$file" =~ c\+\+/codeforces/([0-9]+)/([a-z]+)/sol\.cpp ]]; then
+    if [[ "$file" =~ c\+\+/codeforces/([0-9]+)/([a-z]+[0-9]?)/sol\.cpp ]]; then
         contest_id="${BASH_REMATCH[1]}"
         problem_id="${BASH_REMATCH[2]}"
 
         git add "$file"
         git commit -m "Solve codeforces, contest $contest_id, problem $problem_id" \
-                   -m "problem link: https://codeforces.com/problemset/problem/$contest_id/$problem_id"
+          -m "problem link: https://codeforces.com/problemset/problem/$contest_id/$problem_id" \
+          > /dev/null 2>&1
 
+        echo "$file 커밋 완료"
         commit_made=true
     else
         echo "패턴에 맞지 않음: $file"
@@ -22,8 +24,8 @@ while read -r file; do
 done < <(git ls-files --others --exclude-standard)
 
 if [ "$commit_made" = true ]; then
-    echo "commit 완료, push 진행할게"
-    git push -f origin master
+    git push -f origin master > /dev/null 2>&1
+    echo "push 완료"
 else
     echo "커밋할 파일 없음."
 fi
