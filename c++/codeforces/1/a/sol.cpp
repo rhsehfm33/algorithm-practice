6145d57476624ee1769d3df3a3fe00a63c0efce5
rhsehfm33 <rhsehfm33@gmail.com>
2025-09-09 12:35:10 +0000

    Solve codeforces, contest 1, problem a
    problem link: https://codeforces.com/problemset/problem/1/a

---
+++ c++/codeforces/1/a/sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+
+int _ti;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (_ti = 1; _ti <= T; ++_ti) {
+        solve();
+    }
+
+    return 0;
+}
